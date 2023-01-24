dd3c6029da78c8d675154f782bc7ff4c5db0412b
Beka00best <bukhatovb@mail.ru>
2023-01-24 16:24:29 +0600

    add instructions

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# transferring_style_bot
+# Style Transfer Service
+![Image alt](https://github.com/Beka00best/transferring_style_bot/static/neuralstyle.png)
+
+This project is to transfer the style of one image to another image.
+
+## Discription
+
+This is a Telegram bot code that implements a slow and fast transfer of your style from a picture to your photo.
+
+The slow algorithm was taken as a basis from this <a href="https://arxiv.org/abs/1508.06576">article</a>. And the implementation was taken from the official <a href="https://pytorch.org/tutorials/advanced/neural_style_tutorial.html">pytorch tutorial</a>. The full description can be viewed by clicking on the links above.
+
+A fast algorithm using the Msg-Net neural network described in <a href="https://arxiv.org/pdf/1703.06953.pdf ">this article</a>. The training code and all other experiments were taken <a href="https://github.com/zhanghang1989/PyTorch-Multi-Style-Transfer ">from here</a>. I trained msg-net on a very large number of content images from the COCO validation dataset, about 6GB of images and 21 stylistic ones. But in the end, the network has learned to copy not only these styles, but also any (or almost any) other! For more information, see <a href="https://github.com/Beka00best/transferring_style_bot/blob/main/train_gan/train.ipynb">train.ipynb</a>
+
+## Stages:
+1. [Make a telegram bot](#Make-a-telegram-bot) :white_check_mark: 
+2. [Build a ready-made GAN](#Build-a-ready-made-GAN) :white_check_mark: 
+3. [Train and embed your own GAN](#Train-and-embed-your-own-GAN) :white_check_mark: 
+4. [Docker](#Docker) :white_check_mark: 
+6. [Tests](#Tests) :white_check_mark: 
+
+## Download
+Install the dependencies
+```sh
+git clone https://github.com/Beka00best/transferring_style_bot.git
+cd transferring_style_bot
+```
+Then you have to add config.py with bot token. Token create with BotFather. In config.py add:
+```sh
+TOKEN = ""
+```
+### Docker
+```sh
+docker build -t prog .
+```
+Viewing images
+```sh
+docker images
+```
+### Run
+```sh
+docker run prog
+```
+### Other way
+If you encounter problems with Docker, you can download it in a different way. You need to have a Linux system. 
+1. You need python3
+2. You need pip
+In the terminal, enter the following commands:
+```sh
+pip install -r requirements.txt
+python3 bot.py
+```
+
+### How use bot
+After starting the bot.py go to telegram:
+```sh
+/start
+```
+
+You will be taken to this page:
+![Image alt](https://github.com/Beka00best/transferring_style_bot/static/1.jpeg)
+Then follow the instructions. Press "See an example":
+![Image alt](https://github.com/Beka00best/transferring_style_bot/static/2.jpeg)
+Then press another buttons, and follow the instructions:
+![Image alt](https://github.com/Beka00best/transferring_style_bot/static/3.jpeg)
+![Image alt](https://github.com/Beka00best/transferring_style_bot/static/4.jpeg)
+
+
+### Clean
+Delete all images
+```sh
+docker rmi -f $(docker images -a -q)
+```
+Stop all containers
+```sh
+docker stop $(docker ps -a -q)
+```
+Delete all containers
+```sh
+docker rm $(docker ps -a -f status=exited -q)
+```
--- bot.py
+++ bot.py
@@ -21,36 +21,36 @@ def welcome(message):
     bot.send_sticker(message.chat.id, sti)
 
     bot.send_message(message.chat.id,
-                     'Добро пожаловать! \n Я - {1.first_name}! \n'.format(message.from_user, bot.get_me()),
+                     'Hello, my friend! \n I\'m - {1.first_name}! \n'.format(message.from_user, bot.get_me()),
                      parse_mode='html')
-    bot.send_message(message.chat.id, 'Я могу переносить стили из одной картинки на другую')
+    bot.send_message(message.chat.id, 'I can transfer styles from one image to another')
     choice(message)
 
 
 def choice(message):
     moon = types.InlineKeyboardMarkup(row_width=1)
-    man1 = types.InlineKeyboardButton('Начать работу с первой моделью nst', callback_data='start1')
-    man2 = types.InlineKeyboardButton('Начать работу с второй моделью msg_net', callback_data='start2')
-    man3 = types.InlineKeyboardButton('Посмотреть пример', callback_data='look')
+    man1 = types.InlineKeyboardButton('Getting started with the first nst model', callback_data='start1')
+    man2 = types.InlineKeyboardButton('Getting started with the second msg_net model', callback_data='start2')
+    man3 = types.InlineKeyboardButton('See an example', callback_data='look')
 
     moon.add(man1, man2, man3)
 
-    bot.send_message(message.chat.id, 'Можно посмотреть пример, либо начать работу', reply_markup=moon)
+    bot.send_message(message.chat.id, 'You can see an example, or get started', reply_markup=moon)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == 'start1' or call.data == 'start2':
-        bot.send_message(call.message.chat.id, 'Загрузи первую картинку')
+        bot.send_message(call.message.chat.id, 'Upload the first picture')
         bot.register_next_step_handler(call.message, handle_photo_content, call.data)
     elif call.data == 'look':
-        bot.send_message(call.message.chat.id, 'Бот просит \'Загрузи первую картинку\'. Мы загружаем картинку, на которую мы хотим нанести новый стиль')
+        bot.send_message(call.message.chat.id, 'The bot asks \'Upload the first picture\'. We upload a picture on which we want to apply a new style')
         bot.send_photo(call.message.chat.id, photo=open("media/example_content_img.jpg", 'rb'))
-        bot.send_message(call.message.chat.id, 'Далее бот просит \'Загрузи стилевую картинку\'. Мы загружаем вторую картинку уже со стилем')
+        bot.send_message(call.message.chat.id, 'Next, the bot asks \'Upload a style picture\'. We upload the second image already with the style')
         bot.send_photo(call.message.chat.id, photo=open("media/example_style_img.jpg", 'rb'))
-        bot.send_message(call.message.chat.id, 'После некоторого ожидания бот нам выводит картинку с перенесенным стилем')
+        bot.send_message(call.message.chat.id, 'After some waiting, the bot displays a picture with the transferred style to us')
         bot.send_photo(call.message.chat.id, photo=open("media/example_output_img.jpg", 'rb'))
-        bot.send_message(call.message.chat.id, 'Усё')
+        bot.send_message(call.message.chat.id, 'That all')
         choice(call.message)
 
 
@@ -61,7 +61,7 @@ def handle_photo_content(message, command):
     src = os.path.join(os.getcwd(), 'media', 'content_input.jpg')
     with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
-    bot.send_message(message.chat.id, "Загрузи стилевую картинку")
+    bot.send_message(message.chat.id, "Upload a style picture")
     bot.register_next_step_handler(message, handle_photo_style, command)
 
 
@@ -71,7 +71,7 @@ def handle_photo_style(message, command):
     src = os.path.join(os.getcwd(), 'media', 'style_input.jpg')
     with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
-    bot.send_message(message.chat.id, "Процесс начат...")
+    bot.send_message(message.chat.id, "The process has started...")
     if command == 'start1':
         nst_model()
         bot.send_photo(message.chat.id, photo=open(src_output, 'rb'))
